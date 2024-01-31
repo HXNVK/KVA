b4277121114eb2f7f0a2c20a0779ca17651db231
Michael Lantzen <michael.lantzen@deltakonzept.de>
2024-01-31 14:58:25 +0100

    optimize

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return "Blah";
-    //return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return "Blah";
+//     //return $request->user();
+// });
